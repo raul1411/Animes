9733fd795783a08e3ae8d8a9d1ccb6cc51ba7fa9
iam06301971 <raulgrandelopez1411@gmail.com>
2021-11-22 21:00:19 +0100

    callate 8

--- src/main/java/com/example/anime/controller/AnimeController.java
+++ src/main/java/com/example/anime/controller/AnimeController.java
@@ -1,15 +1,13 @@
 package com.example.anime.controller;
 
-import com.example.anime.domain.dto.ListResult;
 import com.example.anime.domain.model.Anime;
 import com.example.anime.repository.AnimeRepository;
-import org.apache.coyote.Response;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/anime")
@@ -20,8 +18,11 @@ public class AnimeController {
     public AnimeController(AnimeRepository animeRepository) { this.animeRepository = animeRepository; }
 
     @GetMapping("/")
-    public ResponseEntity<?> findAllAnimes() {
-        return ResponseEntity.ok().body(ListResult.list(animeRepository.findAll()));
+    public ResponseEntity<?> findAllAnimes(@PathVariable UUID id) {
+        Anime file = animeRepository.findById(id).orElse(null);
+
+        if(file==null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Error.message("No s'ha trobat l'anime amb ID: " + id));
+        return ResponseEntity.ok().body(file);
     }
 
     @PostMapping("/{id}")
---
+++ src/main/java/com/example/anime/domain/dto/Error.java
@@ -0,0 +1,12 @@
+package com.example.anime.domain.dto;
+
+public class Error {
+    public String message;
+
+    public static Error message(String message) {
+        Error error = new Error();
+        error.message =message;
+        return error;
+    }
+
+}
