0fe13655815221b6c7796edc94cf1e903ce6db29
iam06301971 <raulgrandelopez1411@gmail.com>
2021-11-22 19:48:29 +0100

    callate 2

--- src/main/java/com/example/anime/AnimeApplication.java
+++ src/main/java/com/example/anime/AnimeApplication.java
@@ -1,4 +1,11 @@
 package com.example.anime;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AnimeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeApplication.class, args);
+    }
 }
